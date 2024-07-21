7f825a802ce89c12394e9cb8fa597e37bcfbf3ce
Raphael <camblong.raphael@epitech.eu>
2024-07-22 01:58:52 +0200

    [ADD] custom hooks for filters

---
+++ src/lib/useFilters.tsx
@@ -0,0 +1,25 @@
+import { useMemo, useState } from "react";
+
+interface Filter<T = any> {
+  [key: string]: T;
+}
+
+const useFilters = <T extends Filter>(
+  initial: T = {} as T
+): [T, (modifier: (filters: T) => T) => void, () => void] => {
+  const [filter, setFilter] = useState<T>(initial);
+  const initialFilter = useMemo(() => initial, []);
+
+  const updateFilter = (modifier: (filters: T) => T) => {
+    setFilter(modifier(filter));
+  };
+
+  const resetFilter = () => {
+    setFilter(initialFilter);
+  };
+
+  return [filter, updateFilter, resetFilter];
+};
+
+export default useFilters;
+export type { Filter };
