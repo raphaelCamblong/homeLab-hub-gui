8919da073acd5e559b84153a9d2fbeeab3e9269b
Raphael <camblong.raphael@epitech.eu>
2024-07-22 02:01:39 +0200

    [ADD] basic cluster filters and card

--- src/app/cluster/page.tsx
+++ src/app/cluster/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface ClusterProps {
   name: string;
@@ -7,8 +8,21 @@ interface ClusterProps {
 const Cluster: React.FC<ClusterProps> = ({ name }) => {
   return (
     <div>
-      <h1>Welcome to the Cluster component, {name}!</h1>
-      {/* Add your content here */}
+      <h1 className="font-ppneuemachina text-3xl">Cluster</h1>
+      <div className="flex flex-row flex-grow justify-between">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="metric">Metrics</TabsTrigger>
+            <TabsTrigger value="statistic">Statistic</TabsTrigger>
+          </TabsList>
+          <TabsContent value="metric">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="statistic">
+            Change your password here.
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 };
